55e2d85df15ab9b42f4e277cc1c8b061d334d6c6
Kayode Ogidan <kaybanks.developer@gmail.com>
2021-09-21 22:08:48 +0300

    added afew more video extension to the list

--- background.js
+++ background.js
@@ -36,6 +36,8 @@ chrome.browserAction.onClicked.addListener(() => {
         'mp4',
         'webm',
         'flv',
+        'FLV',
+        'ts',
         'mov',
         'ogv',
         '3gp',
@@ -87,54 +89,49 @@ chrome.contextMenus.onClicked.addListener((info) => {
   if (navigator.webdriver !== true) {
     const page = getManifest().homepage_url;
     const { name, version } = getManifest();
-    onInstalled.addListener(
-      ({ reason, previousVersion }) => {
-        management.getSelf(
-          ({ installType }) =>
-            installType === 'normal' &&
-            storage.local.get(
-              {
-                faqs: true,
-                'last-update': 0,
-              },
-              (prefs) => {
-                if (
-                  reason === 'install' ||
-                  (prefs.faqs && reason === 'update')
-                ) {
-                  const doUpdate =
-                    (Date.now() - prefs['last-update']) /
-                      1000 /
-                      60 /
-                      60 /
-                      24 >
-                    45;
-                  if (
-                    doUpdate &&
-                    previousVersion !== version
-                  ) {
-                    tabs.create({
-                      url:
-                        page +
-                        '?version=' +
-                        version +
-                        (previousVersion
-                          ? '&p=' + previousVersion
-                          : '') +
-                        '&type=' +
-                        reason,
-                      active: reason === 'install',
-                    });
-                    storage.local.set({
-                      'last-update': Date.now(),
-                    });
-                  }
+    onInstalled.addListener(({ reason, previousVersion }) => {
+      management.getSelf(
+        ({ installType }) =>
+          installType === 'normal' &&
+          storage.local.get(
+            {
+              faqs: true,
+              'last-update': 0,
+            },
+            (prefs) => {
+              if (
+                reason === 'install' ||
+                (prefs.faqs && reason === 'update')
+              ) {
+                const doUpdate =
+                  (Date.now() - prefs['last-update']) /
+                    1000 /
+                    60 /
+                    60 /
+                    24 >
+                  45;
+                if (doUpdate && previousVersion !== version) {
+                  tabs.create({
+                    url:
+                      page +
+                      '?version=' +
+                      version +
+                      (previousVersion
+                        ? '&p=' + previousVersion
+                        : '') +
+                      '&type=' +
+                      reason,
+                    active: reason === 'install',
+                  });
+                  storage.local.set({
+                    'last-update': Date.now(),
+                  });
                 }
               }
-            )
-        );
-      }
-    );
+            }
+          )
+      );
+    });
     setUninstallURL(
       page +
         '?rd=feedback&name=' +
